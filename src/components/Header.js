0d06737ecb10235e76dcd5bd81810f32c03615db
Tolibjon <tolibjohn@gmail.com>
2022-03-16 22:08:09 +0500

    Create Header component

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const Container = styled.div`
+  border: 1px solid #ccc;
+  font-size: 1.5rem;
+`;
+
+const Header = () => {
+  return (
+    <Container>xChange</Container>
+  )
+}
+
+export default Header
