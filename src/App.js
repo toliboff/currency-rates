78b5012b64750fb4e7978ba9744e626dfe56893a
Tolibjon <tolibjohn@gmail.com>
2022-03-16 22:09:45 +0500

    Fetch data from api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import { getCurrentRatings } from './api';
 import './App.css';
+import Currency from './components/Currency';
+import Header from './components/Header';
 
 function App() {
+  const [currencies, setCurrencies] = useState([]);
+
+  useEffect(()=>{
+    const getRatings = async () => {
+         try {
+          const currencyList =  await getCurrentRatings();
+          setCurrencies(Object.values(currencyList.Valute));
+          const l = currencyList;
+          console.log('List: ', Object.entries(l.Valute));
+        }
+         catch (error) {
+        }
+      }
+      getRatings();    
+  },[]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <ul>
+      {
+        currencies && currencies.map((currency) => <li><Currency  currency={currency}/></li>)
+      }
+     
+      </ul>
     </div>
   );
 }
