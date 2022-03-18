9b76ba0fffd696565c3d83fdacf99d463fa80f48
Tolibjon <tolibjohn@gmail.com>
2022-03-18 17:58:19 +0500

    Add params to change content

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,34 @@ import styled from 'styled-components';
 const Container = styled.div`
   border: 1px solid #ccc;
   font-size: 1.5rem;
+  padding: 1rem 2rem;
+  background-color: hsl(194deg 96% 42%);
+  color: #fff;
+  position: fixed;
+  top:0;
+  width:100%;
+  height: 5rem;
 `;
 
-const Header = () => {
+const Title = styled.h1`
+  
+  font-size: 1.5rem;
+`;
+
+const SubTitle = styled.p`
+  
+  font-size: 0.8rem;
+`;
+
+const Header = ({title, subtitle}) => {
   return (
-    <Container>xChange</Container>
+    <Container>
+      <Title>
+        {title}
+      </Title>
+      <SubTitle>{subtitle}</SubTitle>
+     
+    </Container>
   )
 }
 
