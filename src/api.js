c6bd4d3420a4141209203c58523533417ae66617
Tolibjon <tolibjohn@gmail.com>
2022-03-18 18:00:12 +0500

    Add API endpoint and create function to fetch data for selected currency for a given number of days

--- src/api.js
+++ src/api.js
@@ -5,8 +5,30 @@ const BASE_URL = 'https://www.cbr-xml-daily.ru/';
 export const getCurrentRatings = async() => {
   try {
     const res = await axios.get(`${BASE_URL}/daily_json.js`);
-    // console.log(res.data);
     return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export const getDailyRatings = async(days = 10, currencyCode ) => {
+  const rates = [];
+  try {
+    const initialResponce = await axios.get(`${BASE_URL}/daily_json.js`);
+    rates.push({
+                date: initialResponce.data.Date,
+                rate: initialResponce.data.Valute[currencyCode],
+                previous: initialResponce.data.PreviousURL,
+              });
+    for (let i = 0; i < days-1; i++) {
+        const res = await axios.get(rates[i].previous);
+        rates.push({
+                    date:res.data.Date, 
+                    rate: res.data.Valute[currencyCode], 
+                    previous: res.data.PreviousURL
+                  });
+     }
+    return rates;
   } catch (error) {
     console.log(error);
   }
